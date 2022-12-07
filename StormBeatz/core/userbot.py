af3eefedccdf86ee79e9543b41d7cd499607d220
Newmusicyy111 <119903405+Newmusicyy111@users.noreply.github.com>
2022-12-07 13:47:20 +0530

    Update userbot.py

--- StormBeatz/core/userbot.py
+++ StormBeatz/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("StormUpdates")
-                await self.one.join_chat("StormSupportChat")                
+                await self.one.join_chat("NOOBCREATOR")
+                await self.one.join_chat("NOOBXCREATOR")                
             except:
                 pass
             assistants.append(1)
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("StormUpdates")
-                await self.two.join_chat("StormSupportChat")
+                await self.two.join_chat("NOOBCREATOR")
+                await self.two.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(2)
@@ -108,8 +108,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("StormUpdates")
-                await self.three.join_chat("StormSupportChat")
+                await self.three.join_chat("NOOBCREATOR")
+                await self.three.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(3)
@@ -138,8 +138,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("StormUpdates")
-                await self.four.join_chat("StormSupportChat")
+                await self.four.join_chat("NOOBCREATOR")
+                await self.four.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(4)
@@ -168,8 +168,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("StormUpdates")
-                await self.five.join_chat("StormSupportChat")
+                await self.five.join_chat("NOOBCREATOR")
+                await self.five.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(5)
